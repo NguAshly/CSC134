ea1dbfbc159b46ab05fcedcb795bb224a779108d
NguAshly <156948056+NguAshly@users.noreply.github.com>
2024-01-31 19:11:28 +0000

    M2lAB1_Ngu

---
+++ M2LAB1_Ngu.cpp
@@ -0,0 +1,54 @@
+// CSC 134
+//M2LAB1 - Areas
+// Ngu 
+// 1/31/24
+
+#include <iostream> 
+using namespace std;
+
+int main ()  {
+    cout << " part 1 - area of a rectangle" << endl;
+    // declare variables
+    double length;
+    double width;
+    //double height; // for later
+    double area;
+
+    // get input
+    cout << "What is the length of the rectangle? ";
+    cin >> length; 
+    cout << "What is the width of rectangle? ";
+    cin >> width;
+
+    // do calculations
+    area = length * width;
+    // print output
+    cout << "The area of he rectangle is: " << area << endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
