53c053863a39da0cd4b3b426c3ca5a925c9c5bd0
NguAshly <156948056+NguAshly@users.noreply.github.com>
2024-03-18 17:52:38 +0000

    checked in all files

---
+++ M1HW1_Ngu.cpp
@@ -0,0 +1,24 @@
+// CSC 134
+//M1HW1 
+// Ngu 
+// 1/31/24
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string movieName = "Titanic";
+    int releaseYear = 1997;
+    double worldwideGross = 2.18; // in billions
+
+    cout << "The movie \"" << movieName << "\" came out in " << releaseYear << ", and had a worldwide gross of around $" << worldwideGross << " billion." << endl;
+
+    cout << "Rose: \"I'll never let go, Jack. I'll never let go.\"" << endl;
+    cout << "Jack: \"I'm the king of the world!\"" << endl;
+    cout << "Narrator: \"I figure life's a gift and I don't intend on wasting it. You don't know what hand you're gonna get dealt next. You learn to take life as it comes at you... to make each day count.\"" << endl;
+    cout << "Rose: \"I'm flying, Jack!\"" << endl;
+
+    return 0;
+}
---
+++ M3LAB2_Ngu.cpp
@@ -0,0 +1,24 @@
+// CSC 134
+//M2LAB1 - Areas
+// Ngu 
+// 1/31/24
+
+#include <iostream> 
+using namespace std;
+
+int main() {
+int score1;
+double grade;
+// Get the two test scores;
+cout << "Enter grade ";
+cin >> score1;
+
+// Calculate and display the grade score;
+if (score1 >=90) { 
+    cout << "your grade is A";
+}
+if (score1 >=80) {
+    cout << "your grade is B";
+}
+}
+
